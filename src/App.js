7dfa31f1e4c31264d9b89a93ff9a141cdec5ce32
Kristian Skog <skogkristian@nordicmorning.com>
2020-10-13 21:10:38 +0200

    First init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [dataResponse, setDataResponse] = useState('');
+
+  const fetchData = async () => {
+    const data = await fetch("https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY");
+    const response = await data.json()
+
+    const parsedArray = response.near_earth_objects.map((item) => [item.absolute_magnitude_h, item.name])
+
+    setDataResponse(parsedArray)
+  }
+
+  console.log(dataResponse)
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          dataResponse && dataResponse.map((item) => {
+            return (<><h1>{item[0]}</h1>
+              <p>{item[1]}</p> </>)
+          })
+        }
+        {/* {dataResponse.map((item)=>{
+          console.log(item)
+        })} */}
+        <button onClick={fetchData}>click me</button>
       </header>
     </div>
   );
